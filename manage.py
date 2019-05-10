26faec8b7fd308e28113ba3fb7661895d4b376b9
bolgarun21 <bolgarun21@gmail.com>
2019-05-10 16:07:16 +0300

    new

--- manage.py
+++ manage.py
@@ -4,6 +4,6 @@ from flask_script import Server, Manager
 
 manager = Manager(app)
 manager.add_command("runserver", Server('0.0.0.0', port=8000))
-	
+
 if __name__ == "__main__":
-	manager.run()
+    manager.run()
